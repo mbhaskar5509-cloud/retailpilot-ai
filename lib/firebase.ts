3dc61d173ebc0609a44409b87362ae03f5a7c619
msunil5509 <msunil5509@gmail.com>
2026-09-18 20:18:28 +0530

    Add Firebase configuration

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqjRrxzfzprqpzC8y3romJad7PnCilBA0",
+  authDomain: "retailpilot-ai.firebaseapp.com",
+  projectId: "retailpilot-ai",
+  storageBucket: "retailpilot-ai.firebasestorage.app",
+  messagingSenderId: "285394605437",
+  appId: "1:285394605437:web:af7b16d1513338f9de2814"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
